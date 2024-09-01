09b6f8a225ebb93df93d92884f9cee875e0481e7
marcosafeitosa <alietoriando@gmail.com>
2024-09-01 13:12:47 -0300

    correcao de bugs

--- src/components/EditModal/EditModal.jsx
+++ src/components/EditModal/EditModal.jsx
@@ -1,133 +1,134 @@
- import React, { useState, useEffect } from 'react';
-  import { Modal, Form, Input, Button, Radio, Typography, Select, Space } from 'antd';
-  import { UserOutlined, LockOutlined, EditOutlined, SettingOutlined } from '@ant-design/icons';
-  import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Modal, Form, Input, Button, Radio, Typography, Select, Space, Card } from 'antd';
+import { UserOutlined, LockOutlined, EditOutlined, SettingOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
 
-  const { Title } = Typography;
-  const { Option } = Select;
+const { Title } = Typography;
+const { Option } = Select;
 
-  const EditModal = ({ open, onCancel, onSave }) => {
-    const [isSecondCompany, setIsSecondCompany] = useState(false);
-    const [form] = Form.useForm(); // Instância de formulário do Ant Design
-    const navigate = useNavigate(); // Hook de navegação para redirecionamento
+const EditModal = ({ open, onCancel, onSave }) => {
+  const [isSecondCompany, setIsSecondCompany] = useState(false);
+  const [form] = Form.useForm(); // Instância de formulário do Ant Design
+  const navigate = useNavigate(); // Hook de navegação para redirecionamento
 
-    // Desabilitar o scroll de fundo quando o modal estiver aberto
-    useEffect(() => {
-      if (open) {
-        document.body.style.overflow = 'hidden';
-      } else {
-        document.body.style.overflow = 'auto';
-      }
+  // Desabilitar o scroll de fundo quando o modal estiver aberto
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
 
-      return () => {
-        document.body.style.overflow = 'auto';
-      };
-    }, [open]);
-
-    useEffect(() => {
-      // Pegar dados salvos e inicializar o formulário
-      const savedData = JSON.parse(localStorage.getItem('loginData'));
-      if (savedData) {
-        form.setFieldsValue(savedData);
-        setIsSecondCompany(savedData.segundaCompanhia === 'sim');
-      }
-    }, [form, open]);
-
-    // Função para verificar se o formulário foi editado
-    const isFormEdited = (currentValues) => {
-      const savedData = JSON.parse(localStorage.getItem('loginData'));
-      return JSON.stringify(currentValues) !== JSON.stringify(savedData);
+    return () => {
+      document.body.style.overflow = 'auto';
     };
+  }, [open]);
 
-    const handleFinish = (values) => {
-      const formData = { ...values };
+  useEffect(() => {
+    // Pegar dados salvos e inicializar o formulário
+    const savedData = JSON.parse(localStorage.getItem('loginData'));
+    if (savedData) {
+      form.setFieldsValue(savedData);
+      setIsSecondCompany(savedData.segundaCompanhia === 'sim');
+    }
+  }, [form, open]);
 
-      // Capturar o texto do conteúdo do option selecionado no Select
-      const patenteSelect = document.querySelector('.ant-select-selection-item');
-      const patenteText = patenteSelect ? patenteSelect.textContent : '';
-      formData.patente = patenteText;
+  // Função para verificar se o formulário foi editado
+  const isFormEdited = (currentValues) => {
+    const savedData = JSON.parse(localStorage.getItem('loginData'));
+    return JSON.stringify(currentValues) !== JSON.stringify(savedData);
+  };
 
-      // Verificar se o usuário escolheu "Demitido"
-      if (formData.patente === 'Demitido') {
-        localStorage.removeItem('loginData'); // Limpar o local storage
-        navigate('/'); // Redirecionar para a tela de login
-        if (typeof onCancel === 'function') {
-          onCancel(); // Fechar o modal
-        }
-        return;
-      }
+  const handleFinish = (values) => {
+    const formData = { ...values };
 
-      // Atualizar dados no local storage
-      localStorage.setItem('loginData', JSON.stringify(formData));
+    // Capturar o texto do conteúdo do option selecionado no Select
+    const patenteSelect = document.querySelector('.ant-select-selection-item');
+    const patenteText = patenteSelect ? patenteSelect.textContent : '';
+    formData.patente = patenteText;
 
-      // Chamar a função de callback para salvar as informações editadas
-      if (typeof onSave === 'function') {
-        onSave(formData);
-      }
+    // Verificar se o usuário escolheu "Demitido"
+    if (formData.patente === 'Demitido') {
+      localStorage.removeItem('loginData'); // Limpar o local storage
+      navigate('/'); // Redirecionar para a tela de login
       if (typeof onCancel === 'function') {
-        onCancel(); // Fechar o modal após salvar
+        onCancel(); // Fechar o modal
       }
-    };
+      return;
+    }
 
-    const handleCancel = () => {
-      const currentValues = form.getFieldsValue();
-      if (!isFormEdited(currentValues)) {
-        if (typeof onCancel === 'function') {
-          onCancel(); // Fechar o modal se o formulário não foi editado
-        }
+    // Atualizar dados no local storage
+    localStorage.setItem('loginData', JSON.stringify(formData));
+
+    // Chamar a função de callback para salvar as informações editadas
+    if (typeof onSave === 'function') {
+      onSave(formData);
+    }
+    if (typeof onCancel === 'function') {
+      onCancel(); // Fechar o modal após salvar
+    }
+  };
+
+  const handleCancel = () => {
+    const currentValues = form.getFieldsValue();
+    if (!isFormEdited(currentValues)) {
+      if (typeof onCancel === 'function') {
+        onCancel(); // Fechar o modal se o formulário não foi editado
       }
-    };
+    }
+  };
 
-    // Definir as patentes disponíveis com base na patente atual
-    const getAvailablePatentes = () => {
-      const savedData = JSON.parse(localStorage.getItem('loginData'));
-      if (!savedData) return []; // Retorna um array vazio se não houver dados salvos
+  // Definir as patentes disponíveis com base na patente atual
+  const getAvailablePatentes = () => {
+    const savedData = JSON.parse(localStorage.getItem('loginData'));
+    if (!savedData) return []; // Retorna um array vazio se não houver dados salvos
 
-      const patenteAtual = savedData.patente; // Pega a patente atual do local storage
-      const patentes = [
-        { value: 'terceiro-sargento', label: 'Terceiro Sargento' },
-        { value: 'segundo-sargento', label: 'Segundo Sargento' },
-        { value: 'primeiro-sargento', label: 'Primeiro Sargento' },
-        { value: 'subtenente', label: 'Subtenente' },
-        { value: 'aluno-espcex', label: 'Aluno da EsPCEx' },
-        { value: 'cadete', label: 'Cadete da AMAN' },
-        { value: 'aspirante', label: 'Aspirante a Oficial' },
-        { value: 'segundo-tenente', label: 'Segundo Tenente' },
-        { value: 'primeiro-tenente', label: 'Primeiro Tenente' },
-        { value: 'capitao', label: 'Capitão' },
-        { value: 'major', label: 'Major' },
-        { value: 'tenente-coronel', label: 'Tenente Coronel' },
-        { value: 'coronel', label: 'Coronel' },
-        { value: 'gdb', label: 'General de Brigada' },
-        { value: 'gdd', label: 'General de Divisão' },
-        { value: 'gde', label: 'General de Exército' },
-        { value: 'demitido', label: 'Demitido' },
-      ];
+    const patenteAtual = savedData.patente; // Pega a patente atual do local storage
+    const patentes = [
+      { value: 'terceiro-sargento', label: 'Terceiro Sargento' },
+      { value: 'segundo-sargento', label: 'Segundo Sargento' },
+      { value: 'primeiro-sargento', label: 'Primeiro Sargento' },
+      { value: 'subtenente', label: 'Subtenente' },
+      { value: 'aluno-espcex', label: 'Aluno da EsPCEx' },
+      { value: 'cadete', label: 'Cadete da AMAN' },
+      { value: 'aspirante', label: 'Aspirante a Oficial' },
+      { value: 'segundo-tenente', label: 'Segundo Tenente' },
+      { value: 'primeiro-tenente', label: 'Primeiro Tenente' },
+      { value: 'capitao', label: 'Capitão' },
+      { value: 'major', label: 'Major' },
+      { value: 'tenente-coronel', label: 'Tenente Coronel' },
+      { value: 'coronel', label: 'Coronel' },
+      { value: 'gdb', label: 'General de Brigada' },
+      { value: 'gdd', label: 'General de Divisão' },
+      { value: 'gde', label: 'General de Exército' },
+      { value: 'demitido', label: 'Demitido' },
+    ];
 
-      // Encontrar o índice da patente atual e retornar apenas as patentes acima dela
-      const indexAtual = patentes.findIndex(p => p.label === patenteAtual);
-      if (indexAtual !== -1) {
-        return patentes.slice(indexAtual); // Retorna apenas as patentes iguais ou superiores
-      }
+    // Encontrar o índice da patente atual e retornar apenas as patentes acima dela
+    const indexAtual = patentes.findIndex(p => p.label === patenteAtual);
+    if (indexAtual !== -1) {
+      return patentes.slice(indexAtual); // Retorna apenas as patentes iguais ou superiores
+    }
 
-      return patentes; // Retorna todas as patentes se não houver correspondência (fallback)
-    };
+    return patentes; // Retorna todas as patentes se não houver correspondência (fallback)
+  };
 
-    return (
-      <Modal
-        title={<Title level={3}><EditOutlined /> Editar Informações</Title>}
-        open={open} // Alterar para open
-        onCancel={handleCancel}
-        footer={null}
-        destroyOnClose={true}  // Destruir o modal ao fechar para evitar bugs de formulário
-      >
+  return (
+    <Modal
+      title={<Title level={3}><EditOutlined /> Editar Informações</Title>}
+      open={open} // Alterar para open
+      onCancel={handleCancel}
+      footer={null}
+      destroyOnClose={true}  // Destruir o modal ao fechar para evitar bugs de formulário
+      width={600}  // Ajuste o tamanho do modal para ser mais largo
+    >
+      <Card bordered={false} style={{ margin: '0 auto' }}>
         <Form
           form={form} // Associar o formulário com a instância de formulário Ant Design
           name="editForm"
           initialValues={{ remember: true }}
           layout="vertical"
           onFinish={handleFinish}
-          style={{ maxWidth: 400, margin: '0 auto' }}
         >
           <Form.Item
             name="nick"
@@ -142,7 +143,7 @@
             label="Patente"
             rules={[{ required: true, message: 'Por favor, selecione sua patente!' }]}
           >
-            <Select placeholder="Selecione sua patente" id="patente">
+            <Select placeholder="Selecione sua patente" id="patente" allowClear>
               {getAvailablePatentes().map((patente) => (
                 <Option key={patente.value} value={patente.value}>
                   {patente.label}
@@ -193,8 +194,9 @@
             </Button>
           </Form.Item>
         </Form>
-      </Modal>
-    );
-  };
+      </Card>
+    </Modal>
+  );
+};
 
-  export default EditModal;
+export default EditModal;
--- src/components/PaginaTreinos/PaginaTreinos.jsx
+++ src/components/PaginaTreinos/PaginaTreinos.jsx
@@ -25,6 +25,7 @@ const PaginaTreinos = () => {
   const handleSave = (values) => {
     const loginData = JSON.parse(localStorage.getItem("loginData")) || {};
     Object.assign(loginData, values);
+
     localStorage.setItem("loginData", JSON.stringify(loginData));
     // window.location.reload(); // Recarrega a página para exibir as informações atualizadas
   };
@@ -39,40 +40,98 @@ const PaginaTreinos = () => {
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <Content style={{ padding: "0px", display: "flex", justifyContent: "center" }}>
-        <Card style={{ width: "100%", maxWidth: "600px", height: "max-content", padding: "1px" }}>
+      <Content
+        style={{ padding: "0px", display: "flex", justifyContent: "center" }}
+      >
+        <Card
+          style={{
+            width: "100%",
+            maxWidth: "600px",
+            height: "max-content",
+            padding: "1px",
+          }}
+        >
           <Row align="middle" gutter={16}>
             <Col>
               <Avatar size={54} icon={<UserOutlined />} />
             </Col>
             <Col flex="auto">
               <Typography>
-                <Row style={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: "100%" }}>
+                <Row
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    width: "100%",
+                  }}
+                >
                   <Title level={4} style={{ marginBottom: 0 }}>
                     {loginData.nick || "Usuário"}
                   </Title>
-                  <Button type="primary" icon={<LogoutOutlined />} onClick={handleLogout}>
+                  <Button
+                    type="primary"
+                    icon={<LogoutOutlined />}
+                    onClick={handleLogout}
+                  >
                     Sair
                   </Button>
                 </Row>
-                <div style={{ marginTop: "10px", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                <div
+                  style={{
+                    marginTop: "10px",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
                   <div>
-                    <Text strong style={{ marginRight: "5px" }}>Patente:</Text>
+                    <Text strong style={{ marginRight: "5px" }}>
+                      Patente:
+                    </Text>
                     {loginData.patente || "Não definido"}
                   </div>
-                  <Button type="link" icon={<EditOutlined />} onClick={() => handleEditClick("patente")} style={{ marginLeft: "8px" }} />
+                  <Button
+                    type="link"
+                    icon={<EditOutlined />}
+                    onClick={() => handleEditClick("patente")}
+                    style={{ marginLeft: "8px" }}
+                  />
                 </div>
-                <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
                   <div>
-                    <Text strong>Supervisor:</Text> {loginData.supervisor ? "Sim" : "Não"}
+                    <Text strong>Supervisor:</Text>{" "}
+                    {loginData.supervisor === 'sim' ? 'Sim' : 'Não'}
                   </div>
-                  <Button type="link" icon={<EditOutlined />} onClick={() => handleEditClick("supervisor")} style={{ marginLeft: "8px" }} />
+                  <Button
+                    type="link"
+                    icon={<EditOutlined />}
+                    onClick={() => handleEditClick("supervisor")}
+                    style={{ marginLeft: "8px" }}
+                  />
                 </div>
-                <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
                   <div>
-                    <Text strong>Segunda Companhia:</Text> {formatSecondCompany(loginData.segundaCompanhia)}
+                    <Text strong>Segunda Companhia:</Text>{" "}
+                    {formatSecondCompany(loginData.segundaCompanhia)}
                   </div>
-                  <Button type="link" icon={<EditOutlined />} onClick={() => handleEditClick("segundaCompanhia")} style={{ marginLeft: "8px" }} />
+                  <Button
+                    type="link"
+                    icon={<EditOutlined />}
+                    onClick={() => handleEditClick("segundaCompanhia")}
+                    style={{ marginLeft: "8px" }}
+                  />
                 </div>
               </Typography>
             </Col>
