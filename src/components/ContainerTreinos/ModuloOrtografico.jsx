eb94bf6f1df9f24bf66694a5fa5beac51219a76e
Marcos Davi A. Feitosa <89201085+marcosafeitosa@users.noreply.github.com>
2024-11-22 20:06:05 -0300

    Update ModuloOrtografico.jsx

--- src/components/ContainerTreinos/ModuloOrtografico.jsx
+++ src/components/ContainerTreinos/ModuloOrtografico.jsx
@@ -1,41 +1,212 @@
-import React from 'react';
-import { Card, Typography } from 'antd';
-import { LeftOutlined } from '@ant-design/icons'; // Importa o ícone de seta para a esquerda
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from "react";
+import { Card, Typography, Button, notification } from "antd";
+import {
+  LeftOutlined,
+  ArrowLeftOutlined,
+  ArrowRightOutlined,
+} from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+import Clipboard from "clipboard";
 
 const { Title, Text } = Typography;
 
-const ModuloOrtografico = () => {
-  const navigate = useNavigate();
+const paragrafoArray = [
+"I - INTRODUÇÃO (Balão Verde)"
+];
 
-  // Função para lidar com a navegação ao clicar no ícone
+const patenteMap = {
+  "Terceiro Sargento": "Sargento",
+  "Segundo Sargento": "Sargento",
+  "Primeiro Sargento": "Sargento",
+  "Subtenente": "Subtenente",
+  "Aluno da EsPCEx": "Aluno",
+  "Cadete da AMAN": "Cadete",
+};
+
+const ModuloOrtografico = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [loginData, setLoginData] = useState(null);
+  const navigate = useNavigate();
+  const copyButtonRef = useRef(null);
+  const paragraphRefs = useRef([]); // Adiciona um array de referências para os parágrafos
+
   const handleBack = () => {
-    navigate('/pagina-treinos'); // Substitua pela rota desejada
+    navigate("/pagina-treinos");
   };
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("loginData"));
+    setLoginData(storedData);
+  }, []);
+
+  // Função para remover "(Balão Verde)" do texto
+  const removeBalãoVerde = (text) => {
+    return text.replace(/\(Balão Verde\)$/, "").trim();
+  };
+
+  // Função para obter o texto do parágrafo atualizado
+  const getParagraphText = (index) => {
+    if (loginData) {
+      if (index === 2) {
+        return `Eu sou o ${loginData.patente} ${loginData.nick} e irei treiná-lo(a) para a sua aprovação.`;
+      }
+      if (index === 4) {
+        if (loginData.corpoDeOficiais === "sim") {
+          return 'Responda às perguntas usando "Sim, Senhor", e "Não, Senhor".';
+        }
+        if (loginData.corpoDePracas === "sim") {
+          const patente = loginData.patente;
+          const tratamento = patenteMap[patente] || "Senhor";
+          return `Responda às perguntas usando "Sim, ${tratamento}", e "Não, ${tratamento}".`;
+        }
+      }
+    }
+    return paragrafoArray[index];
+  };
+
+  const copyToClipboard = (index) => {
+    const text = getParagraphText(index);
+    const processedText = removeBalãoVerde(text);
+
+    if (navigator.clipboard) {
+      navigator.clipboard
+        .writeText(processedText)
+        .then(() => {
+          notification.success({
+            message: "Texto copiado para a área de transferência!",
+            duration: 0.5,
+          });
+        })
+        .catch((err) => {
+          notification.error({
+            message: "Falha ao copiar o texto.",
+            description: err.toString(),
+            duration: 0.5,
+          });
+        });
+    } else if (copyButtonRef.current) {
+      const clipboard = new Clipboard(copyButtonRef.current, {
+        text: () => processedText,
+      });
+      clipboard.on("success", () => {
+        notification.success({
+          message: "Texto copiado para a área de transferência!",
+          duration: 0.5,
+        });
+        clipboard.destroy();
+      });
+      clipboard.on("error", (err) => {
+        notification.error({
+          message: "Falha ao copiar o texto.",
+          description: err.toString(),
+          duration: 0.5,
+        });
+        clipboard.destroy();
+      });
+      copyButtonRef.current.click();
+    }
+  };
+
+  const handlePrevious = () => {
+    const newIndex = currentIndex > 0 ? currentIndex - 1 : 0;
+    setCurrentIndex(newIndex);
+    copyToClipboard(newIndex);
+  };
+
+  const handleNext = () => {
+    const newIndex =
+      currentIndex < paragrafoArray.length - 1
+        ? currentIndex + 1
+        : paragrafoArray.length - 1;
+    setCurrentIndex(newIndex);
+    copyToClipboard(newIndex);
+  };
+
+  // Efeito para centralizar o parágrafo atual na tela
+  useEffect(() => {
+    if (paragraphRefs.current[currentIndex]) {
+      paragraphRefs.current[currentIndex].scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  }, [currentIndex]);
 
   return (
     <Card
       bordered={false}
-      style={{ width: '100%', height: '100%', textAlign: 'center', position: 'relative' }}
+      style={{
+        width: "100%",
+        height: "100%",
+        textAlign: "center",
+        position: "relative",
+      }}
     >
-      {/* Ícone de Voltar */}
       <LeftOutlined
         onClick={handleBack}
         style={{
-          fontSize: '20px',
-          position: 'absolute',
-          color: 'red',
+          fontSize: "20px",
+          position: "absolute",
+          color: "red",
           left: 10,
-          bottom: 10,
-          cursor: 'pointer',
-        }} // Estilo para o ícone
+          bottom: 60,
+          cursor: "pointer",
+        }}
       />
 
-      {/* Conteúdo do Card */}
-      <Title level={2}>MO</Title>
-      <Text type="secondary" style={{ fontSize: '12px' }}>
-        Treinamento Básico I
+      <Title level={2}>T3</Title>
+      <Text type="secondary" style={{ fontSize: "12px" }}>
+        Treinamento Complementar I
       </Text>
+
+      <div
+        style={{
+          marginTop: "20px",
+          textAlign: "justify",
+          maxWidth: "600px",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      >
+        {paragrafoArray.map((texto, index) => (
+          <p
+            key={index}
+            ref={(el) => (paragraphRefs.current[index] = el)} // Adiciona a referência ao parágrafo
+            style={{
+              marginBottom: "10px",
+              padding: "10px",
+              backgroundColor:
+                index === currentIndex
+                  ? "#d9d9d9a3"
+                  : texto.includes("(Balão Verde)")
+                  ? "#d4edda"
+                  : "transparent",
+              borderRadius: "4px",
+            }}
+          >
+            {getParagraphText(index)}
+          </p>
+        ))}
+      </div>
+
+      <div
+        style={{
+          position: "fixed",
+          bottom: 20,
+          left: "50%",
+          transform: "translateX(-50%)",
+          display: "flex",
+          gap: "20px",
+        }}
+      >
+        <Button icon={<ArrowLeftOutlined />} onClick={handlePrevious}>
+          Anterior
+        </Button>
+        <Button icon={<ArrowRightOutlined />} onClick={handleNext}>
+          Próximo
+        </Button>
+        <button ref={copyButtonRef} style={{ display: "none" }}></button>
+      </div>
     </Card>
   );
 };
